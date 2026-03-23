f46aaa1c6335769287c6e90c41417d9e3bc5772c
Jamal Taha <jamaltaha197@gmail.com>
2026-03-23 13:57:05 +0100

    Add files via upload

---
+++ mein j.py
@@ -0,0 +1,5 @@
+print("\nWelcome To the youtube cganal Name Generator:\n")
+nick_name = input("What is your  Nickname?\n")
+chanal_name = input("What is your chanal name?\n")
+
+print(f"You could name your chanal ({chanal_name} with {nick_name})")
